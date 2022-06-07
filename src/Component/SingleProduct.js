2be464b6320a3b9c5d401fcb9155a624a3c6b5da
sahilsinghnayal <sahilsinghnayal@gmail.com>
2022-06-07 13:31:36 +0530

    cart adding

--- src/Component/SingleProduct.js
+++ src/Component/SingleProduct.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
+import { CartState } from "../Context/Context";
 import Rating from "./Rating";
 
 function SingleProduct({ prod }) {
+  const{state:{cart},dispatch,}=CartState();
   return (
     <div className="products">
       <Card>
@@ -18,12 +20,31 @@ function SingleProduct({ prod }) {
             )}
             <Rating rating={prod.ratings} />
           </Card.Subtitle>
-          <Button variant="danger">
-            Remove from cart
-          </Button>
-          <Button disabled={!prod.inStock}>
-           {!prod.inStock?"out of stock":"Add to cart"} 
-          </Button>
+          {cart.some((p) => p.id === prod.id) ? (
+            <Button
+              variant="danger"
+              onClick={() =>
+                dispatch({
+                  type: "REMOVE_FROM_CART",
+                  payload: prod,
+                })
+              }
+            >
+              Remove from Cart
+            </Button>
+          ) : (
+            <Button
+              onClick={() =>
+                dispatch({
+                  type: "ADD_TO_CART",
+                  payload: prod,
+                })
+              }
+              disabled={!prod.inStock}
+            >
+              {!prod.inStock ? "Out of Stock" : "Add to Cart"}
+            </Button>
+          )}
         </Card.Body>
        
       </Card>
